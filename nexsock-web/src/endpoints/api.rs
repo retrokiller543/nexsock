b07017e6a873312c5dac63bc7696a7cec8ab1c73
Emil Schütt <emil.schutt@skolon.com>
2025-01-24 10:54:37 +0100

    Added support for sending env variables when starting a service via the website
    Signed-off-by: Emil Schütt <emil.schutt@skolon.com>

--- nexsock-web/src/endpoints/api.rs
+++ nexsock-web/src/endpoints/api.rs
@@ -2,18 +2,30 @@ use crate::{connect_to_client, AppState};
 use anyhow::anyhow;
 use axum::extract::{Path, State};
 use axum::response::{IntoResponse, Redirect};
+use axum::Json;
 use nexsock_protocol::commands::manage_service::{
     ServiceRef, StartServiceCommand, StopServiceCommand,
 };
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
 
-async fn start_service_inner(state: Arc<AppState>, service_ref: ServiceRef) -> anyhow::Result<()> {
+#[derive(Deserialize)]
+pub struct StartServiceForm {
+    env_keys: Vec<String>,
+    env_values: Vec<String>,
+}
+
+async fn start_service_inner(
+    state: Arc<AppState>,
+    service_ref: ServiceRef,
+    env_vars: HashMap<String, String>,
+) -> anyhow::Result<()> {
     let mut client = connect_to_client(state.config.socket()).await?;
 
     let res = client
-        .execute_command(StartServiceCommand::new(service_ref, HashMap::new()))
+        .execute_command(StartServiceCommand::new(service_ref, env_vars))
         .await?;
 
     if res.is_error() {
@@ -26,10 +38,11 @@ async fn start_service_inner(state: Arc<AppState>, service_ref: ServiceRef) -> a
 pub(crate) async fn start_service(
     State(state): State<Arc<AppState>>,
     Path(service_ref): Path<String>,
+    Json(env_vars): Json<HashMap<String, String>>,
 ) -> impl IntoResponse {
     let service_ref = ServiceRef::from_str(service_ref.as_str()).unwrap();
 
-    start_service_inner(state.clone(), service_ref.clone())
+    start_service_inner(state.clone(), service_ref.clone(), env_vars)
         .await
         .unwrap();
 
